9b19d8a723af1a1ed8a73d1f16f2a7a98092601c
selvagsz <selvaganeshbsg@gmail.com>
2017-01-23 18:33:08 +0530

    Turn TypeAhead/SelectTrigger into pure functional component

--- src/TypeAhead/SelectTrigger.js
+++ src/TypeAhead/SelectTrigger.js
@@ -1,63 +1,43 @@
 import React, { Component } from 'react'
 
-export default class TypeAheadSelectTrigger extends Component {
-  constructor() {
-    super(...arguments)
-    this.handleOnFocus = ::this.handleOnFocus
-    this.handleOnBlur = ::this.handleOnBlur
+export default (props) => {
+  let {
+    selectedOption,
+    selectedLabel,
+    placeholder,
+    disabled,
+    onClick,
+    handleOnChange,
+    handleKeyDown,
+    handleOnFocus,
+    handleOnBlur,
+  } = props
+
+  let value = props.searchTerm
+  if (value === null) {
+    value = selectedOption ? selectedOption[selectedLabel] : ''
   }
 
-  handleOnFocus(event) {
-    this.props.select.open()
-  }
-
-  handleOnBlur(event) {
-    let select = this.props.select
-    setTimeout(function() {
-      select.close()
-    }, 300)
-  }
-
-  render() {
-    let props = this.props
-    let {
-      selectedOption,
-      selectedLabel,
-      placeholder,
-      disabled,
-      onClick,
-      handleOnChange,
-      handleKeyDown,
-      handleOnFocus,
-      handleOnBlur,
-    } = props
-
-    let value = this.props.searchTerm
-    if (value === null) {
-      value = selectedOption ? selectedOption[selectedLabel] : ''
-    }
-
-    return (
-      <div
-        className='powerselect__trigger'
-        onClick={onClick}
-      >
-        <input
-          className='powerselect__trigger-input'
-          autoComplete='off'
-          spellCheck='false'
-          placeholder={props.placeholder}
-          value={value}
-          disabled={disabled}
-          onChange={handleOnChange}
-          onKeyDown={handleKeyDown}
-          onFocus={handleOnFocus}
-          onBlur={handleOnBlur}
-        />
-        <span
-          className='powerselect__trigger-status'
-        ></span>
-      </div>
-    )
-  }
+  return (
+    <div
+      className='powerselect__trigger'
+      onClick={onClick}
+    >
+      <input
+        className='powerselect__trigger-input'
+        autoComplete='off'
+        spellCheck='false'
+        placeholder={props.placeholder}
+        value={value}
+        disabled={disabled}
+        onChange={handleOnChange}
+        onKeyDown={handleKeyDown}
+        onFocus={handleOnFocus}
+        onBlur={handleOnBlur}
+      />
+      <span
+        className='powerselect__trigger-status'
+      ></span>
+    </div>
+  )
 }
