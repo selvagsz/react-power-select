dcf9d1520bf5837033f076b1bd8b0ba503df2864
selvagsz <selvaganeshbsg@gmail.com>
2017-05-24 11:45:47 +0530

    Fix matcher for string option

--- src/utils.js
+++ src/utils.js
@@ -1,7 +1,7 @@
 export const matcher = ({ option, searchTerm = '', searchIndices }) => {
   searchTerm = searchTerm.trim().toLowerCase()
   if (typeof option === 'string') {
-    return option.indexOf(searchTerm) !== -1
+    return option.toLowerCase().indexOf(searchTerm) !== -1
   }
   if (searchIndices) {
     return searchIndices.some((index) => {
